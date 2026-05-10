9ced7663657265443c5b17449bc56080d9b206fe
Viktor Gustafsson <viktor.gustafsson@bannerflow.com>
2026-05-10 12:12:24 +0200

    add new game codes

--- data.js
+++ data.js
@@ -3,40 +3,160 @@ let data = {
         totalButtonCount: 8,
         response: 301818
     },
+    2074: {
+        totalButtonCount: 8,
+        response: 736291
+    },
+    3215: {
+        totalButtonCount: 8,
+        response: 174283
+    },
+    1546: {
+        totalButtonCount: 8,
+        response: 384176
+    },
+    1059: {
+        totalButtonCount: 8,
+        response: 462857
+    },
     5690: {
         totalButtonCount: 9,
         response: 660918
+    },
+    7421: {
+        totalButtonCount: 9,
+        response: 487293
+    },
+    6018: {
+        totalButtonCount: 9,
+        response: 273849
+    },
+    5408: {
+        totalButtonCount: 9,
+        response: 367194
+    },
+    7128: {
+        totalButtonCount: 9,
+        response: 256849
+    },
+    5926: {
+        totalButtonCount: 9,
+        response: 263571
     },
     3167: {
         totalButtonCount: 10,
         response: 292678
     },
-    2985: {
-        totalButtonCount: 11,
-        response: 915716
-    },
-    1193: {
-        totalButtonCount: 12,
-        response: 120192
-    },
-    1985: {
-        totalButtonCount: 13,
-        response: 481504
-    },
-    4819: {
-        totalButtonCount: 14,
-        response: 793530
-    },
     6743: {
         totalButtonCount: 10,
         response: 965925
+    },
+    9637: {
+        totalButtonCount: 10,
+        response: 318652
+    },
+    1764: {
+        totalButtonCount: 10,
+        response: 459137
+    },
+    9174: {
+        totalButtonCount: 10,
+        response: 248516
+    },
+    5079: {
+        totalButtonCount: 10,
+        response: 415738
+    },
+    6371: {
+        totalButtonCount: 10,
+        response: 184562
+    },
+    2985: {
+        totalButtonCount: 11,
+        response: 915716
     },
     1908: {
         totalButtonCount: 11,
         response: 696615
     },
+    8356: {
+        totalButtonCount: 11,
+        response: 152864
+    },
+    8902: {
+        totalButtonCount: 11,
+        response: 826394
+    },
+    6789: {
+        totalButtonCount: 11,
+        response: 716295
+    },
+    9805: {
+        totalButtonCount: 11,
+        response: 348729
+    },
+    3407: {
+        totalButtonCount: 11,
+        response: 859124
+    },
+    1193: {
+        totalButtonCount: 12,
+        response: 120192
+    },
     4983: {
         totalButtonCount: 12,
         response: 235875
+    },
+    3849: {
+        totalButtonCount: 12,
+        response: 596418
+    },
+    2861: {
+        totalButtonCount: 12,
+        response: 815732
+    },
+    3964: {
+        totalButtonCount: 12,
+        response: 872194
+    },
+    8714: {
+        totalButtonCount: 12,
+        response: 718394
+    },
+    1985: {
+        totalButtonCount: 13,
+        response: 481504
+    },
+    5182: {
+        totalButtonCount: 13,
+        response: 904175
+    },
+    7693: {
+        totalButtonCount: 13,
+        response: 952648
+    },
+    8237: {
+        totalButtonCount: 13,
+        response: 629583
+    },
+    4682: {
+        totalButtonCount: 13,
+        response: 935681
+    },
+    4819: {
+        totalButtonCount: 14,
+        response: 793530
+    },
+    4527: {
+        totalButtonCount: 14,
+        response: 681524
+    },
+    4350: {
+        totalButtonCount: 14,
+        response: 593481
+    },
+    2493: {
+        totalButtonCount: 14,
+        response: 591736
     }
 }
